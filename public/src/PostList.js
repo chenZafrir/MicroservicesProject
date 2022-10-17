06e4a1e8521401dba3d0805a5a16650f0017538b
Chen <chen20032@gmail.com>
2022-10-17 18:44:51 +0300

    CommentCreate implementation

--- public/src/PostList.js
+++ public/src/PostList.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
+import CommentCreateComponent from "./CommentCreate";
+import CommentListComponent from "./CommentList";
 
 const PostListComponent = () => {
     const [posts, setPosts] = useState({});
@@ -17,11 +19,13 @@ const PostListComponent = () => {
         return <div className="card" style={{width: '30%', marginBottom:'20px'}} key={post.id}>
             <div className="card-body">
                 <h3>{post.title}</h3>
+                <CommentListComponent postId={post.id}/>
+                <CommentCreateComponent postId={post.id}/>
             </div>
         </div>
     });
 
-    return <div className="d-flex flex-row flex-wrap justify-content-between">
+    return <div className="d-flex flex-row flex-wrap justify-content-between">     
         {renderedPosts}
     </div>
 }
